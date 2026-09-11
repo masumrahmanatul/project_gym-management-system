f2ac47f719bb4c48600b1c0dd1f36ae814b90324
masumrahmanatul <143866312+masumrahmanatul@users.noreply.github.com>
2026-09-11 17:36:43 +0600

    Delete project practice directory

--- project practice/StartGUI.java
+++
@@ -1,9 +0,0 @@
-import Entity.*;
-import EntityList.*;
-import GUI.*;
-
-public class StartGUI{
-	public static void main(String[] args){
-		BookManagementPage bmp = new BookManagementPage();
-	}
-}
